0c6317a258de7bbb94c05925d5cc1ed49f8825d0
Grigore <grisha2609@gmail.com>
2026-04-03 16:34:15 +0200

    erste commit

---
+++ src/main/java/com/grigore/klassenbuch/api/KlassenbuchApiApplication.java
@@ -0,0 +1,13 @@
+package com.grigore.klassenbuch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlassenbuchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlassenbuchApiApplication.class, args);
+    }
+
+}
