47b64a6df6736d2e5bf2dd09ed1a2da3f574e379
Grigore <grisha2609@gmail.com>
2026-04-04 14:23:15 +0200

    alle controllers und services hinzugefugt, dependencies erros solved

---
+++ src/main/java/com/grigore/klassenbuch/api/config/OpenApiConfig.java
@@ -0,0 +1,4 @@
+package com.grigore.klassenbuch.api.config;
+
+public class OpenApiConfig {
+}
---
+++ src/main/java/com/grigore/klassenbuch/api/controller/LerntagController.java
@@ -0,0 +1,4 @@
+package com.grigore.klassenbuch.api.controller;
+
+public class LerntagController {
+}
